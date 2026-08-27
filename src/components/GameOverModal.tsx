a6b98c0de02b9f2a456fe978cbbb7eceaa2ea51b
Jack Wu <wmshellfaith@gmail.com>
2026-08-28 00:48:41 +0800

    Fix unused import in GameOverModal

--- src/components/GameOverModal.tsx
+++ src/components/GameOverModal.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { AnswerRecord } from '../types/game';
 import { getBadgeForScore } from '../utils/gameLogic';
 import { playTickSound, playScoreSound } from '../utils/audio';
